3d684dfe5cac9e20cbb9a8fd2d2172432792f47d
coppieee <coppieee@gmail.com>
2021-09-26 17:17:54 +0900

    library update

--- app.js
+++ app.js
@@ -1,45 +1,37 @@
 "use strict";
-var express = require('express')
-  , routes = require('./routes')
-  , http = require('http')
-  , path = require('path');
+var express = require("express"),
+  routes = require("./routes"),
+  http = require("http"),
+  path = require("path");
+var socketIO = require("socket.io");
 
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+var port = 3000;
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', routes.index);
+app.get("/", routes.index);
 
 var server = http.createServer(app);
-server.listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+
+server.listen(port, function () {
+  console.log("Express server listening on port " + port);
 });
 
 //socket.ioのインスタンス作成
-var io = require('socket.io').listen(server);
+var io = new socketIO.Server(server);
 
 //クライアントから接続があった時
-io.sockets.on('connection',function(socket){
-	//クライアントからmessageイベントが受信した時
-	socket.on('message',function(data){
-		//念のためdataの値が正しいかチェック
-		if(data && typeof data.text === 'string'){
-			//メッセージを投げたクライアント以外全てのクライアントにメッセージを送信する。
-			socket.broadcast.json.emit('message',{text:data.text});
-		}
-	});
+io.sockets.on("connection", function (socket) {
+  //クライアントからmessageイベントが受信した時
+  socket.on("message", function (data) {
+    //念のためdataの値が正しいかチェック
+    if (data && typeof data.text === "string") {
+      //メッセージを投げたクライアント以外全てのクライアントにメッセージを送信する。
+      socket.broadcast.emit("message", { text: data.text });
+    }
+  });
 });
